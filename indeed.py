5e400bc365b3ecf5605294e444a0a11b48b065bd
erikapereira <pereiraerika@outlook.com>
2020-01-18 18:55:47 +0000

    added additional query param

--- indeed.py
+++ indeed.py
@@ -4,8 +4,10 @@ from bs4 import BeautifulSoup
 import re
 
 
-payload = {'key1': 'value1', 'key2': 'value2'}
-parameters = ['product owner', 'product manager', 'business analyst']
+payload1 = {'key1': 'value1', 'key2': 'value2'}
+parameters = ['product owner']
+
+              # 'product manager', 'business analyst']
 
 job_keys = []
 count = 0
@@ -13,22 +15,20 @@ jobs = []
 
 for parameter in parameters:
 
-    payload = {'q': parameter}
+    payload = {'q': parameter, 'l': 'remote'}
     url = "https://www.indeed.co.uk/jobs"
     r = requests.get(url, params=payload)
     results = re.findall(r"jobKeysWithInfo\['([0-9a-f]+)'\]", r.text)
-
-    # for key in results:
     job_keys.extend(results)
 
-# print(job_keys)
+
 
 for job_key in job_keys:
 
     url = 'https://www.indeed.co.uk/viewjob?jk=' + job_key
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "lxml")
-    results = soup.findAll('a')
+    # results = soup.findAll('a')
     # i also want to update db with this url
     count += 1
     if count > 3:
@@ -36,9 +36,14 @@ for job_key in job_keys:
 
     sleep(0.5)
 
-    for result in results:
-        if 'london' in result.text.lower():
-            print(result)
+    print(url)
+
+
+
+
+
+
+
 
 
 
