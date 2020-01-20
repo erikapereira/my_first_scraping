eb6f65b0af6e499cc5098cd86ce8054d3aa1642b
erikapereira <pereiraerika@outlook.com>
2020-01-20 11:15:11 +0000

    dealing with pagination

--- indeed.py
+++ indeed.py
@@ -4,22 +4,26 @@ from bs4 import BeautifulSoup
 import re
 
 
-payload1 = {'key1': 'value1', 'key2': 'value2'}
-parameters = ['product owner']
-
-# ,'product manager', 'business analyst']
-
+parameters = ['manager', 'product owner']
+limit = 10
 job_keys = []
 count = 0
 
+# limit : number of results 10/20/30/50
+# count : limiter on number of job_key urls
+
+
 for parameter in parameters:
 
-    payload = {'q': parameter, 'l': 'remote'}
-    url = "https://www.indeed.co.uk/jobs"
-    r = requests.get(url, params=payload)
-    results = re.findall(r"jobKeysWithInfo\['([0-9a-f]+)'\]", r.text)
-    job_keys.extend(results)
-
+    for page in range (0, 40, limit):
+        # range function! page number starts at 0 and increases by limit
+        payload = {'q': parameter, 'limit': limit, 'start': page}
+        url = "https://www.indeed.co.uk/jobs"
+        r = requests.get(url, params=payload)
+        results = re.findall(r"jobKeysWithInfo\['([0-9a-f]+)'\]", r.text)
+        job_keys.extend(results)
+        # extend adds to job_keys []
+        print(job_keys)
 
 
 for job_key in job_keys:
@@ -29,25 +33,11 @@ for job_key in job_keys:
     soup = BeautifulSoup(r.text, "lxml")
     job_title = soup.find('h3')
 
-
     count += 1
-    if count > 20:
+    if count > 3:
         break
-
+        # will only generate upto x urls & stop
     sleep(0.5)
 
     print(job_title.text)
     print(url)
-    # i also want to update db with this url
-
-
-
-
-
-
-
-
-
-
-
-
